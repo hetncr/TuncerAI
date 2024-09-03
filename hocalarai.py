15b733d10e49a253d0533f092d64b719c2a1f7d1
hetncr <97675352+hetncr@users.noreply.github.com>
2024-09-03 17:47:56 +0300

    Update hocalarai.py

--- hocalarai.py
+++ hocalarai.py
@@ -48,7 +48,7 @@ def extract_text_from_excel(uploaded_file):
 
 # Streamlit app
 st.title("**:blue[HOCALAR AI]**")
-st.subheader("**:blue:point_right:[Beyin Bedava]:point_left:**", divider='rainbow')
+st.subheader("**:blue::point_right:[Beyin Bedava]:point_left:**", divider='rainbow')
 
 # Input for API key
 api_key = st.text_input('Google Gemini API anahtarını giriniz:', type='password')
